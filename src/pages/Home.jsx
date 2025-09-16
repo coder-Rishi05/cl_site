288d09572ea43ed01a224c938812b3dbe7f92a08
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-16 20:58:08 +0530

    completed second page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import Nav from "./Nav";
+import Offring from "./Offring";
 
 const Home = () => {
   return (
-    <div className=" h-screen w-full bg-[#654ecd1a] ">
+    <div className="  w-full  bg-[#654ecd1a] ">
       <Nav />
-      <div className="left">
-        <h1 className="text-[#423385] text-[2.4rem] px-5 leading-[3rem] py-8 font-ubuntu font-bold tracking-tight text-center ">
-        Never Have a Empty Sales Pipeline!
-      </h1>
-      <p className="text-3xl px-6 text-[#423385] leading-4xl text-center">
-        Imagine having a People-Independent B2B LeadGen Engine using AI
-      </p>
-      <button className="px-6 py-4 my-12 text-2xl  font-medium text-center border-1 rounded-3xl mx-6 border-[#f87661] text-[#f87661]">
-        See the 8-Step Revenue System Design
-      </button>
-      </div>
-      <div className="right">
-        
-      </div>
+      <main className="flex flex-col h-full w-screen items-center lg:flex-row justify-between">
+        <div className="left">
+          <h1 className="text-[#423385] text-[2.4rem] px-5 leading-[3rem] py-8 font-ubuntu font-bold tracking-tight text-center ">
+            Never Have a Empty Sales Pipeline!
+          </h1>
+          <p className="text-3xl px-6 text-[#423385] leading-4xl text-center">
+            Imagine having a People-Independent B2B LeadGen Engine using AI
+          </p>
+          <button className="px-6 py-4 my-12 text-2xl  font-medium text-center border-1 rounded-3xl mx-6 border-[#f87661] text-[#f87661]">
+            See the 8-Step Revenue System Design
+          </button>
+        </div>
+        <div className="right  w-full mt-20 px-5  ">
+          <img
+            className="object-fit object-center w-full"
+            src="./Images/rightImg.png"
+            alt=""
+          />
+        </div>
+      </main>
+      <Offring />
     </div>
   );
 };
---
+++ src/pages/Offring.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Offring = () => {
+  return (
+    <div className="w-full px-5 h-screen  ">
+      <div className="top">
+        <h2 className="text-2xl">Our</h2>
+        <h1 className="text-4xl mt-5 text-ubuntu font-bold text-[#423385]">
+          Offrings
+        </h1>
+      </div>
+      <div className="boxes w-full my-10 py-10  flex items-center flex-col gap-5 lg:flex-row">
+        <div className="left h-full rounded-4xl bg-[#DAD1FF]">
+          <div className="center flex   flex-col gap-2 py-8 px-10  ">
+            <h1 className="text-3xl  font-bold">Setup & Stretagy</h1>
+            <h3 className="text-[1.6rem] ">
+              Upskill your revenue generation department
+            </h3>
+            <button className="px-8 py-3 bg-white border-1 border-amber-600 text-[1.6rem] underline text-[#423385]">
+              Training Pricing
+            </button>
+            <img
+              className=" object-fit object-cover"
+              src="./Images/bt-1.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="right h-full rounded-4xl bg-[#2A207B]">
+          <div className="center flex   flex-col gap-2 py-8 px-10  ">
+            <h1 className="text-3xl text-white  font-bold">Setup & Stretagy</h1>
+            <h3 className="text-[1.6rem] text-white ">
+              Setup system and build your team.
+            </h3>
+            <button className="px-8 py-3 bg-white border-1 border-amber-600 text-[1.6rem] underline text-[#423385]">
+              Training Pricing
+            </button>
+            <img
+              className=" object-fit object-cover"
+              src="./Images/bt-1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offring;
