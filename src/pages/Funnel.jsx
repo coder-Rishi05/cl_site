b688fb62f66f5a4110b36c99b9ee670d5565718d
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-18 21:27:14 +0530

    completed funnel page

--- src/pages/Funnel.jsx
+++ src/pages/Funnel.jsx
@@ -2,21 +2,131 @@ import React from "react";
 
 const Funnel = () => {
   return (
-    <div className="w-full h-screen bg-red-500 flex flex-col lg:flex-row gap-6 ">
-      <div className="top">
-        <h2>Our Proprietary</h2>
-        <h1>8-Step Full Funnel System</h1>
+    <div className="w-full h-screen flex flex-col lg:flex-row gap-6 ">
+      <div className="top px-6  ">
+        <h2 className="text-2xl text-blue-700">Our Proprietary</h2>
+        <h1 className="text-3xl my-5 text-[#0F114D] font-bold">
+          8-Step Full Funnel System
+        </h1>
       </div>
-      <div className="boxes  ">
-        <div className="box1 bg-blue-500 rounded-xl ">
-          <h1 className="text-xl text-white ">
-            1. Message-Market Fit for Leads
-          </h1>
-          <p className="text-xl text-white ">
-            Timeline : upto 12 weeks for U.S
-          </p>
+      <div className="boxes px-6 flex flex-col gap-6 ">
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
           <div className="image w-[20rem] ">
-            <img className="object-cover object-center " src="./Images/message.png" alt="" />
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
+          <div className="image w-[20rem] ">
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
+          <div className="image w-[20rem] ">
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
+          <div className="image w-[20rem] ">
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
+          <div className="image w-[20rem] ">
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
+          <div className="image w-[20rem] ">
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="box1  flex flex-col gap-12 px-4  bg-[#3325A2] rounded-4xl ">
+          <div className="texts">
+            <h1 className="text-3xl px-2 font-bold py-6 text-white ">
+              1. Message-Market Fit for Leads
+            </h1>
+            <p className="text-[2rem]  leading-10  text-white ">
+              Timeline : upto 12 weeks for U.S
+            </p>
+          </div>
+          <div className="image w-[20rem] ">
+            <img
+              className="object-cover object-center "
+              src="./Images/message.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
