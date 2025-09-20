1bfb475e8108a5c3985db39a56dc057096f309dc
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-20 21:32:48 +0530

    completed offring  page responsiveness

--- src/pages/Offring.jsx
+++ src/pages/Offring.jsx
@@ -9,14 +9,16 @@ const Offring = () => {
           Offrings
         </h1>
       </div>
-      <div className="boxes w-full  my-10 py-10 md:py-2  flex items-center flex-col gap-5 lg:flex-row">
-        <div className="left  rounded-4xl bg-[#DAD1FF]">
-          <div className="center flex   flex-col gap-2 py-8 px-10  ">
-            <h1 className="text-3xl  font-bold">Setup & Stretagy</h1>
-            <h3 className="text-[1.6rem] ">
+      <div className="boxes w-full  my-10  py-10 md:py-2  flex items-center flex-col gap-4  lg:flex-row">
+        <div className="left   rounded-4xl bg-[#DAD1FF]">
+          <div className="center flex flex-col gap-2   py-8  px-10  ">
+            <h1 className="text-3xl md:font-medium   font-bold">
+              Setup & Stretagy
+            </h1>
+            <h3 className="text-[1.6rem] md:text-2xl ">
               Upskill your revenue generation department
             </h3>
-            <button className="px-8 py-3 bg-white border-1 border-amber-600 text-[1.6rem] underline text-[#423385]">
+            <button className="px-8 md:mx-auto md:ml-0  py-2 bg-white border-1 border-amber-600 text-[1.6rem] underline text-[#423385]">
               Training Pricing
             </button>
             <img
@@ -26,13 +28,15 @@ const Offring = () => {
             />
           </div>
         </div>
-        <div className="right rounded-4xl bg-[#2A207B]">
+        <div className="right  rounded-4xl bg-[#2A207B]">
           <div className="center flex   flex-col gap-2 py-8 px-10  ">
-            <h1 className="text-3xl text-white  font-bold">Setup & Stretagy</h1>
-            <h3 className="text-[1.6rem] text-white ">
+            <h1 className="text-3xl md:font-medium text-white  font-bold">
+              Setup & Stretagy
+            </h1>
+            <h3 className="text-[1.6rem] md:text-2xl text-white ">
               Setup system and build your team.
             </h3>
-            <button className="px-8 py-3 bg-white border-1 border-amber-600 text-[1.6rem] underline text-[#423385]">
+            <button className="px-8 py-3 md:mx-auto md:ml-0  bg-white border-1 border-amber-600 text-[1.6rem] underline text-[#423385]">
               Training Pricing
             </button>
             <img
